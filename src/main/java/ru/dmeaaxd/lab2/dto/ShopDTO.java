5824b562f3f4c0b7345e470543f5f3c0e687d979
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-05-02 20:34:48 +0300

    update shopDTO: add list categories

--- src/main/java/ru/dmeaaxd/lab2/dto/ShopDTO.java
+++ src/main/java/ru/dmeaaxd/lab2/dto/ShopDTO.java
@@ -1,11 +1,16 @@
 package ru.dmeaaxd.lab2.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
+@Builder
 public class ShopDTO {
     private String name;
     private String description;
+    private List<Long> categories;
 
     public boolean antiCheckerRegister() {
         if (name == null || name.isEmpty()) return true;
